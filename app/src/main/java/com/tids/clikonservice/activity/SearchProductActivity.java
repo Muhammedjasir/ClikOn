ca6d20fbb907aa914c570fa75ccdda1cdb3a4c65
adil <adil.farhan@techlogica.com>
2021-09-01 16:01:39 +0530

    01/09/2021 Adil Farhan - SearchProductActivity layout added

---
+++ app/src/main/java/com/tids/clikonservice/activity/SearchProductActivity.java
@@ -0,0 +1,16 @@
+package com.tids.clikonservice.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tids.clikonservice.R;
+
+public class SearchProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_product);
+    }
+}
